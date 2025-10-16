b099980e45493c22457328ee79cd293dec624760
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-10-16 14:45:38 +0200

    settato rotte crud

---
+++ routers/posts.js
@@ -0,0 +1,31 @@
+// importo express
+const express = require("express");
+
+// settiamo il router
+const router = express.Router();
+
+// index
+router.get('/', function (req, res) {
+    res.send('Lista dei post');
+});
+// show
+router.get('/:id', function (req, res) {
+    res.send('Dettagli del post ' + req.params.id);
+});
+// create
+router.post('/', function (req, res) {
+    res.send('Creazione di un post');
+});
+
+// update
+router.put('/:id', function (req, res) {
+    res.send('Modifica integrale di un post ' + req.params.id);
+});
+
+// destroy
+router.delete('/:id', function (req, res) {
+    res.send('Eliminazione di un post ' + req.params.id);
+});
+
+// esporto router
+module.exports = router;
